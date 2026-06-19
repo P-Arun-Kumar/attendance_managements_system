b16b67ce9953bdde8fe0bf20bd8045ea9eb7687a
P Arun Kumar <parun3333@gmail.com>
2026-06-19 14:21:01 +0530

    js basics

---
+++ Frontend/javascript-practice.js
@@ -0,0 +1,56 @@
+// console.log("welcome students")
+// var a=10
+// var b="10"
+// console.log(a===b)
+// var name = "Arun"
+// var name =" Phani"
+// name = "Vamshi"
+// console.log(name)
+// var name = "kumar"
+// console.log(name)
+// name= "arun"
+// console.log(name)
+
+// function greet () {
+//     var a = "Welcome"
+//     console.log(a)
+// }
+// console.log(a)
+
+// if(true){
+//     let c = 100
+//     console.log(c)
+// }
+// console.log(c)
+// let a = 200
+// a =500
+
+// console.log(typeof null)
+
+// let a=10
+// a +=10 
+// console.log(a)
+// console.log(++a)
+// console.log(a)
+
+
+// const a = 10 
+// console.log(typeof a) 
+// const b = "10"
+// console.log(typeof b) 
+// console.log(a===b)
+
+// const c =30 
+// const checking = c > 50 ? "yes" : "no" 
+// console.log(checking)
+
+
+const age = 12
+if (age >=18) {
+    console.log("elgible to vote")
+}
+else {
+    console.log("")
+}
+
+// take three numbers and print which number is greater amoung three
