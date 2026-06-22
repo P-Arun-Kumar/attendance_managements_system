dfe7461e6e208aea0c3a992e2629d7c620f21abd
P Arun Kumar <parun3333@gmail.com>
2026-06-22 12:55:15 +0530

    dashboard using react

---
+++ Frontend/react-app/src/components/StudentStatus.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function StudentStatus(props) {
+  return (
+    <tr>
+        <td>{props.rollno}</td>
+        <td>{props.name}</td>
+        <td>{props.status}</td>
+    </tr>
+  )
+}
+
+export default StudentStatus
